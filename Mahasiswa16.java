c213e9402624d8d8c7108a61edd036e215743d54
Deizan <deyzen1107@gmail.com>
2026-02-21 12:58:06 +0700

    percobaan 3

--- Mahasiswa16.java
+++ Mahasiswa16.java
@@ -1,12 +1,22 @@
 public class Mahasiswa16 {
 
     String nim;
-    String nama;
+    String nm;
     String kelas;
     double ipk;
     
+    Mahasiswa16() {
+    }
+    
+    Mahasiswa16(String nm, String nim, double ipk, String kelas) {
+        this.nm = nm;
+        this.nim = nim;
+        this.kelas = kelas;
+        this.ipk = ipk;
+    }
+    
     void tampilkanInformasi() {
-        System.out.println("Nama: " + nama);
+        System.out.println("Nama: " + nm);
         System.out.println("Nim: " + nim);
         System.out.println("IPK: " + ipk);
         System.out.println("Kelas: " + kelas);
@@ -14,13 +24,13 @@ public class Mahasiswa16 {
     
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
-        System.out.println("Kelas mahasiswa " + nama + " berhasil diubah menjadi: " + kelas);
+        System.out.println("Kelas mahasiswa " + nm + " berhasil diubah menjadi: " + kelas);
     }
     
     void updateIpk(double ipkBaru) {
         if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
             ipk = ipkBaru;
-            System.out.println("IPK mahasiswa " + nama + " berhasil diubah menjadi: " + ipk);
+            System.out.println("IPK mahasiswa " + nm + " berhasil diubah menjadi: " + ipk);
         } else {
             System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
